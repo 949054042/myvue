c3ab422c8730446f1c6830bf8ef5d043561c6b54
unknown <xuq@Bluemobi.cn>
2018-06-06 16:46:13 +0800

    new

--- src/App.vue
+++ src/App.vue
@@ -95,6 +95,9 @@ export default {
 </script>
 
 <style>
+body{
+  -webkit-overflow-scrolling: auto
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
